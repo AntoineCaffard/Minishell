8b7a67323452dd81b0948d4f20e5ca7e12b3da5f
Toma Rebours <trebours@1G6.42angouleme.fr>
2024-02-21 17:38:55 +0100

    modif parsing_cd.c

--- parsing/parsing_cd.c
+++ parsing/parsing_cd.c
@@ -6,20 +6,22 @@
 /*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:39:24 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/21 17:18:06 by trebours         ###   ########.fr       */
+/*   Updated: 2024/02/21 17:37:54 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing_minishell.h"
 
-size_t	ft_tablen(char **tab)
+size_t	ft_stringtab_len(char **tab)
 {
-	int	i;
+	size_t	i;
 
 	if (!tab)
-		exit(1);
+		return (0);
 	i = 0;
 	while (tab[i])
+		i++;
+	return (i);
 }
 
 void	parsing_cd(char **params)
