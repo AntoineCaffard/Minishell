21d66b1e2d916efc74b925a24c9e28a8ea9a1e36
Antoine Caffard <acaffard@1H5.42angouleme.fr>
2024-02-21 17:26:23 +0100

    Modified parsing.h

--- parsing/parsing_minishell.h
+++ parsing/parsing_minishell.h
@@ -6,7 +6,7 @@
 /*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:53:26 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/21 17:16:36 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/02/21 17:26:03 by acaffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define PARSING_H
 
 # include "../includes/LIBFT/libft.h"
-
+# include "../minishell.h"
 
 
 #endif
