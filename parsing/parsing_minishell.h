a5f63243440a7f732d291618416331adf925e7d6
Toma Rebours <trebours@2F3.42angouleme.fr>
2024-02-22 10:50:10 +0100

    update parsing_minishell.h

--- parsing/parsing_minishell.h
+++ parsing/parsing_minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_minishell.h                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:53:26 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/21 17:26:03 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/02/22 10:49:05 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,12 @@
 # include "../includes/LIBFT/libft.h"
 # include "../minishell.h"
 
+void	parsing_cd(char **params);
+void	parsing_pwd(char **params);
+void	parsing_env(char **params);
+void	parsing_echo(char **params);
+void	parsing_exit(char **params);
+void	parsing_unset(char **params);
+void	parsing_export(char **params);
 
 #endif
