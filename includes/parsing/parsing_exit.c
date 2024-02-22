7d1b53c92797f093175ad0cd2b2202b893568569
Antoine Caffard <acaffard@1H5.42angouleme.fr>
2024-02-22 13:25:05 +0100

    FEATURE DONE

--- includes/parsing/parsing_exit.c
+++ includes/parsing/parsing_exit.c
@@ -6,7 +6,14 @@
 /*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:39:04 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/21 16:39:04 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/02/22 13:02:03 by acaffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "parsing_minishell.h"
+
+void	parsing_exit.c(char **params)
+{
+	if (ft_stringtab_len(params) > 1)
+		strerror(0); // a revoir
+}
