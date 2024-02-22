3d147d47be916d49e71ed3a1ad52c81a49a9bcbc
Toma Rebours <trebours@2F3.42angouleme.fr>
2024-02-22 10:37:02 +0100

    delete fonction count_occurences_in_string in parsing_export.c

--- parsing/parsing_export.c
+++ parsing/parsing_export.c
@@ -6,29 +6,11 @@
 /*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:38:44 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/22 10:06:51 by trebours         ###   ########.fr       */
+/*   Updated: 2024/02/22 10:31:40 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing_minishell.h"
-
-int	count_occurences_in_string(const char *src, const char c)
-{
-	int	i;
-	int	count;
-
-	if (!src)
-		return (0);
-	i = 0;
-	count = 0;
-	while (src[i])
-	{
-		if (src[i] == c)
-			count++;
-		i++;
-	}
-	return (count);
-}
 
 void	parsing_export(char **params)
 {
