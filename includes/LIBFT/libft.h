a5c48d115630e17ec1927966d38ccd7f5e8b5d55
Toma Rebours <trebours@2F3.42angouleme.fr>
2024-02-22 10:35:12 +0100

    add count_occurences_in_string.c

--- includes/LIBFT/libft.h
+++ includes/LIBFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 14:07:47 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/22 10:11:54 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/02/22 10:33:45 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -162,5 +162,6 @@ void	ft_free_stringtab(char **to_free);
 size_t	ft_stringtab_len(char **tab);
 void	ft_print_stringtab(char **tab);
 char	**ft_split_str(char const *s, char *separator);
+int     count_occurences_in_string(const char *src, const char c);
 
 #endif
