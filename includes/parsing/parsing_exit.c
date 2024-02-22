b6cfe1e433521677110fb4fb135938b37b75b460
Toma Rebours <trebours@1G6.42angouleme.fr>
2024-02-22 13:27:36 +0100

    fixed parsing_exit.c

--- includes/parsing/parsing_exit.c
+++ includes/parsing/parsing_exit.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:39:04 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/22 13:02:03 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/02/22 13:26:30 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing_minishell.h"
 
-void	parsing_exit.c(char **params)
+void	parsing_exit(char **params)
 {
 	if (ft_stringtab_len(params) > 1)
 		strerror(0); // a revoir
