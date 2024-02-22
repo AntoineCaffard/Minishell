6e36a460cb9b4fbe0bde505e13acefef2061f95a
Toma Rebours <trebours@2F3.42angouleme.fr>
2024-02-22 10:05:06 +0100

    parsing_export.c fait

--- parsing/parsing_export.c
+++ parsing/parsing_export.c
@@ -3,10 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:38:44 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/21 16:38:45 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/02/22 10:03:10 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "parsing_minishell.h"
+
+int	count_occurences_in_string(const char *src, const char c)
+{
+	int	i;
+	int	count;
+
+	if (!src)
+		return (0);
+	i = 0;
+	count = 0;
+	while (src[i])
+	{
+		if (src[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+void	parsing_export(char **params)
+{
+	if (ft_stringtab_len(params) != 1)
+		strerror(0); // a revoir
+	if (count_char_in_string(params[1], '=') > 1)
+		strerror(0); // a revoir
+}
