6689018800d0b5d0536665dab099135b61b5e4aa
Toma Rebours <trebours@2F3.42angouleme.fr>
2024-02-22 10:24:20 +0100

    save parsing_env.c and norm parsing_cd.c parsing_export.c

--- parsing/parsing_env.c
+++ parsing/parsing_env.c
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:39:08 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/21 16:39:09 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/02/22 10:15:25 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "parsing_minishell.h"
+
+void	parsing_env(char **params)
+{
+	int	i;
+
+	if (ft_stringtab_len(params) < 1)
+		strerror(0); // a revoir
+	i = 0;
+	while (params[i])
+	{
+		if (count_occurences_in_string(param[i], '=') > 1)
+		{
+			strerror(0); // a revoir
+			break;
+		}
+		i++;
+	}
+}
