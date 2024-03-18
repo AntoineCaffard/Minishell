9c1cfb118e74d863c86a3931279a0662a36ba5f9
Toma Rebours <trebours@1G6.42angouleme.fr>
2024-03-18 11:28:21 +0100

    fixed error

--- includes/parsing/parsing_echo.c
+++ includes/parsing/parsing_echo.c
@@ -6,11 +6,19 @@
 /*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 11:22:17 by trebours          #+#    #+#             */
-/*   Updated: 2024/03/18 11:23:02 by trebours         ###   ########.fr       */
+/*   Updated: 2024/03/18 11:27:23 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing_minishell.h"
+
+int	check_option(char **params)
+{
+	if (ft_strnstr(params[0], "-n", ft_strlen(params[0])))
+		return (42);
+	else
+		return (0);
+}
 
 void	parsing_echo(char **params)
 {
