5206a12c00dfde4710d9bca708ccb2fb3f2cc409
Antoine Caffard <acaffard@1G7.42angouleme.fr>
2024-03-18 09:38:29 +0100

    Fixed export

--- includes/builtins/export.c
+++ includes/builtins/export.c
@@ -3,22 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
+/*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 09:55:40 by acaffard          #+#    #+#             */
-/*   Updated: 2024/02/28 11:02:37 by antoine          ###   ########.fr       */
+/*   Updated: 2024/03/18 09:32:26 by acaffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static size_t	ft_strlen_until_equal(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] && str[i] != '=')
+		i++;
+	return (i);
+}
+
 static t_list	*get_node_by_value(t_list *list, char *param)
 {
 	while (list)
 	{
-		if (ft_strncmp(((char *) (list->content)), param, ft_strlen(param)) == 0)
+		if (ft_strncmp(((char *) (list->content)), param, ft_strlen_until_equal(param)) == 0)
 		{
-			if (((char *) (list->content))[ft_strlen(param)] == '=')
+			if (((char *) (list->content))[ft_strlen_until_equal(param)] == '=')
 				return (list);
 		}
 		list = list->next;
@@ -64,13 +74,14 @@ void	minishell_export(t_list **envp, char ** params)
 	i = 0;
 	while (params[i])
 	{
-		node = get_node_by_value(*envp, params[i]);
-		printf("%s\n", (char *) node->content);
-		if (node != NULL)
-			actualize_node_value(node, params[i]);
-		else
-			add_new_env_var(envp, params[i]);
-		//printf("%s\n", (char *) node->content);
+		if (ft_strchr(params[i], '='))
+		{
+			node = get_node_by_value(*envp, params[i]);
+			if (node != NULL)
+				actualize_node_value(node, params[i]);
+			else
+				add_new_env_var(envp, params[i]);
+		}
 		i++;
 	}
 }
