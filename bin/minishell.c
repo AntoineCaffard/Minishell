44c40b4fdc23a8d7c6ca81aa240053fab9f035b1
Antoine Caffard <acaffard@1D1.42angouleme.fr>
2024-04-09 15:28:36 +0200

    added main code's structure

--- bin/minishell.c
+++ bin/minishell.c
@@ -6,7 +6,7 @@
 /*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:53:41 by acaffard          #+#    #+#             */
-/*   Updated: 2024/04/09 14:20:26 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/04/09 15:07:27 by acaffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 int	main(int ac, char **av, char **envp)
 {
+	//t_command_line *pouet;
 	char	*line;
 	(void) ac;
 	(void) av;
 	(void) envp;
+	// t_list envp
 	while (1)
 	{
 		line = readline("Minishell V-2.0 : ");
 		add_history(line);
+		//pouet = create_command_line(line, envp);
+		// if(pouet->error_code != NONE)
+		// free line -> free pouet -> break;
+		//executeur
 		free (line);
 	}
 }
