a6f9b4e7b3cc37d1fc28889a30e33bfd27627cb8
AntoineCaffard <caffard.antoine@gmail.com>
2024-04-22 15:01:00 +0200

    Tested recursion and pseudo coded fill command

--- bin/parser.c
+++ bin/parser.c
@@ -6,17 +6,17 @@
 /*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 11:21:53 by antoine           #+#    #+#             */
-/*   Updated: 2024/04/15 11:46:40 by antoine          ###   ########.fr       */
+/*   Updated: 2024/04/22 15:00:04 by antoine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	*fill_struct(t_command_line *res, char *line)
+void	fill_struct(t_command_line *res, char *line)
 {
 	int				i;
 	bool			test_pipe;
-	t_command		command;
+	t_command		*command;
 
 	i = 0;
 	test_pipe = FALSE;
@@ -28,9 +28,30 @@ void	*fill_struct(t_command_line *res, char *line)
 		test_pipe = TRUE;
 	line[i] = '\0';
 	command = create_command();
-	fill_command(command, ft_strdup(line));
+	if (!command)
+		return ;
+	res->error_code = fill_command(command, ft_strdup(line));
 	if (test_pipe)
 		fill_struct(res, &(line[i + 1]));
 }
 
-// fill_command
+int	fill_command(t_command *cmd, char *line)
+{
+	(void) cmd;
+	//lexer
+	// check redirect
+	// yes : call redirection
+	// no : add argument
+	//recurcive add
+	printf("%s\n", line);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_command_line	test;
+
+	(void) ac;
+	fill_struct(&test, av[1]);
+	return (0);
+}
