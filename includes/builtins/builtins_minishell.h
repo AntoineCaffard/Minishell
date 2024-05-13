27729477ea27e6e0d4621fa778987d2ceb010670
Toma Rebours <trebours@2G2.42angouleme.fr>
2024-05-13 13:25:49 +0200

    update exit

--- includes/builtins/builtins_minishell.h
+++ includes/builtins/builtins_minishell.h
@@ -18,7 +18,7 @@
 void	minishell_cd(char *path);
 void	minishell_echo(char **params);
 void	minishell_env(t_list *envp);
-void	minishell_exit(t_command_line *command, char **cmd, t_list *envp);
+void	minishell_exit(t_command_line *command, char ***cmd, t_list *envp);
 void	minishell_pwd(void);
 void	minishell_unset(t_list **envp, char **params);
 void	minishell_export(t_list **envp, char **params);
--- includes/builtins/exit.c
+++ includes/builtins/exit.c
@@ -12,30 +12,19 @@
 
 #include "builtins_minishell.h"
 
-static void	free_value(t_argument *args)
-{
-	while (args)
-	{
-		free(args->value);
-		args = args->next;
-	}
-}
-
-void	minishell_exit(t_command_line *command, char **cmd, t_list *envp)
+void	minishell_exit(t_command_line *command, char ***cmd, t_list *envp)
 {
 	int	save;
 
 	ft_lstclear(&envp, free);
-	free_value(command->commands->args);
-	free(command->commands->args);
-	free(command->commands);
+	free_struct(command);
 	clear_history();
-	if (cmd[1] != NULL && ft_string_isdigit(cmd[1]))
+	if (cmd[0][1] != NULL && ft_string_isdigit(cmd[0][1]))
 	{
-		save = ft_atoi(cmd[1]);
-		ft_free_stringtab(cmd);
+		save = ft_atoi(cmd[0][1]);
+		ft_free_stringtab(cmd[0]);
 		exit(save);
 	}
-	ft_free_stringtab(cmd);
+	ft_free_stringtab(cmd[0]);
 	exit(0);
 }
