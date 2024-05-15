41004370e59d60399c0b9484d6dd3cf505521c6e
trebours <utilisateur@pop-os.localdomain>
2024-05-15 09:30:27 +0200

    norm minishelll.c

--- bin/minishell.c
+++ bin/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Trebours <Trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:53:41 by acaffard          #+#    #+#             */
-/*   Updated: 2024/05/14 14:15:54 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/05/15 09:28:10 by Trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,11 +57,35 @@ void	ft_verif_exit(t_command_line *command_line, t_list **envp)
 	}
 }
 
+void	loop_main(t_command_line *command_line, t_list *envp)
+{
+	t_command_line	cmd_buffer;
+	char			*line;
+
+	while (1)
+	{
+		line = readline("Minishell V-2.0 : ");
+		if (!line)
+			minishell_exit(command_line, NULL, envp);
+		if (line && line[0] != '\0')
+		{
+			add_history(line);
+			fill_struct(command_line, line);
+			fill_redirection(command_line);
+			free (line);
+			cmd_buffer = *command_line;
+			ft_verif_exit(&cmd_buffer, &envp);
+			main_pipe(&cmd_buffer, &envp);
+			free_struct(command_line);
+		}
+		else
+			free(line);
+	}
+}
+
 int	main(int ac, char **av, char **envp)
 {
-	char			*line;
 	t_command_line	command_line;
-	t_command_line	cmd_buffer;
 	t_list			*env;
 
 	(void) ac;
@@ -71,24 +95,6 @@ int	main(int ac, char **av, char **envp)
 	env = init_stringtab_in_t_list(envp);
 	command_line.commands = NULL;
 	command_line.error_code = 0;
-	while (1)
-	{
-		line = readline("Minishell V-2.0 : ");
-		if (!line)
-			minishell_exit(&command_line, NULL, env);
-		if (line && line[0] != '\0')
-		{
-			add_history(line);
-			fill_struct(&command_line, line);
-			fill_redirection(&command_line);
-			free (line);
-			cmd_buffer = command_line;
-			ft_verif_exit(&cmd_buffer, &env);
-			main_pipe(&cmd_buffer, &env);
-			free_struct(&command_line);
-		}
-		else
-			free(line);
-	}
+	loop_main(&command_line, env);
 	ft_lstclear(&env, free);
 }
